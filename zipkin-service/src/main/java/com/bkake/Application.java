315702630a554c8990df30d492d835056d2d56a2
BKAKE <bangaly.kake@gmail.com>
2018-11-04 15:37:39 +0100

    accessibilité de zipkin par decouverte de service via discovery

--- zipkin-service/src/main/java/com/bkake/Application.java
+++ zipkin-service/src/main/java/com/bkake/Application.java
@@ -2,13 +2,14 @@ package com.bkake;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import zipkin.server.internal.EnableZipkinServer;
 
 
 @SpringBootApplication
 @EnableZipkinServer
+@EnableEurekaClient
 public class Application {
-
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
